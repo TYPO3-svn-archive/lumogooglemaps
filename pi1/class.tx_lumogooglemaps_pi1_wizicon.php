0a04cf5c4da2a8ee439839c378f8cd4fe48676c9
retiolum <retiolum@735d13b6-9817-0410-8766-e36946ffe9aa>
2007-12-14 22:20:36 +0000

    Changed copyright and license header.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/lumogooglemaps/trunk@7534 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_lumogooglemaps_pi1_wizicon.php
+++ pi1/class.tx_lumogooglemaps_pi1_wizicon.php
@@ -1,31 +1,31 @@
 <?php
-/***************************************************************
-*  Copyright notice
-*
-*  (c) 2006 Thomas Off <typo3@retiolum.de>
-*  All rights reserved
-*
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
+/*
+ *  Copyright notice
+ *
+ *  (c) 2007 Thomas Off <typo3@retiolum.de>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ */
 
 /**
  * Class that adds the wizard icon.
  *
- * @author	Thomas Off <typo3@retiolum.de>
+ * @author Thomas Off <typo3@retiolum.de>
  */
 
 class tx_lumogooglemaps_pi1_wizicon {
